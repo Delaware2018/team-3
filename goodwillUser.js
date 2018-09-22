828c11d5dd864371e22255436bbf1d68736d75d4
Ikem14 <32718638+Ikem14@users.noreply.github.com>
2018-09-22 01:09:56 -0400

    Add files via upload

--- goodwillUser.js
+++ goodwillUser.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE user (name VARCHAR(255), email VARCHAR(255), phone INT(10), type VARCHAR(10), address VARCHAR(255), reason VARCHAR(255), donationCount INT(11), boughtCount INT(11))";
+  var sql = "CREATE TABLE user (firstName VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), phone INT(10), type VARCHAR(10), address VARCHAR(255), reason VARCHAR(255), donationCount INT(11), boughtCount INT(11), PRIMARY KEY (email))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
